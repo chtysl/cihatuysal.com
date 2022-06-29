9f400515b9af1b243dbcb00e4ab71d071f67ed90
Cihat Uysal <devCihat@gmail.com>
2022-06-29 20:32:23 +0000

    ready for icons

--- src/lib/Hero.svelte
+++ src/lib/Hero.svelte
@@ -1,5 +1,5 @@
 <div class="hero min-h-screen bg-base-200">
-	<div class="hero-content mx-8 flex-col lg:flex-row-reverse">
+	<div class="hero-content flex-col lg:flex-row-reverse">
 		<img src="/cihatuysal.jpeg" class="max-w-sm rounded-lg shadow-2xl" />
 		<div>
 			<h1 class="text-xl font-thin mb-2">Gayrimenkul Danışmanı</h1>
@@ -8,7 +8,7 @@
 				Remax Aqua şubesinde gayrimenkul danışmanlığı yapıyorum. Konyaaltı sahili boyunca yürüyüş
 				yaparak hem hoş vakit geçirip hemde uzmanlık bölgem olan Liman, Gürsü, Altınkum, Kuşkavşağı,
 				Arapsuyu mahalelerindeki mülkler hakkında bilgi ediniyor ve uzmanlığımı her gün daha da
-				pekiştirmiş oluyorum. Aynı zamanda doğup büyüdüğüm Doyran ve çevresinde uzmanlık
+				pekiştirmiş oluyorum. Aynı zamanda doğup büyüdüğüm Doyran ve Döşemealtı çevresindede uzmanlık
 				yapmaktayım. Yatırımlık arsadan, sahil kenarındaki rezidansa uzmanlığımla hertürlü
 				hizmetinizdeyim.
 			</p>
