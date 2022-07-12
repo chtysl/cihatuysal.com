e639adfe19bce3cf5aa9a091073d2dea61cfbd44
Cihat Uysal <devCihat@gmail.com>
2022-07-12 20:13:21 +0000

    basic blog setup

---
+++ src/routes/blog/index.svelte
@@ -0,0 +1,63 @@
+<script context="module">
+	const allPosts = import.meta.glob('./*.{md,svx}');
+
+	let body = [];
+	for (let path in allPosts) {
+		body.push(
+			allPosts[path]().then(({ metadata }) => {
+				return { path, metadata };
+			})
+		);
+	}
+	export const load = async () => {
+		const posts = await Promise.all(body);
+
+		return {
+			props: {
+				posts
+			}
+		};
+	};
+</script>
+
+<script>
+	// @ts-nocheck
+
+	export let posts;
+
+	const dateSortedPosts = posts.slice().sort((post1, post2) => {
+		return new Date(post2.metadata.date) - new Date(post1.metadata.date);
+	});
+</script>
+
+<ul>
+	{#each dateSortedPosts as { path, metadata: { title, date } }}
+		<li>
+			<a href={path.replace('./', '/blog/').replace('.md', '').replace('.svx', '')}>{title}</a>
+			<p class="date">{new Date(date).toDateString()}</p>
+		</li>
+	{/each}
+</ul>
+
+<style>
+	p {
+		margin: 0;
+		font-size: 0.8rem;
+	}
+	li {
+		margin-bottom: 20px;
+	}
+
+	.tag {
+		margin-right: 10px;
+		text-decoration: none;
+		color: #555;
+	}
+	.tag:hover {
+		color: blue;
+	}
+	.date {
+		font-size: 0.7rem;
+		color: gray;
+	}
+</style>
---
+++ src/routes/blog/test.md
@@ -0,0 +1,8 @@
+---
+title: Top Todo Apps
+date: 2021-06-20
+---
+
+# {title}
+
+This is first post
