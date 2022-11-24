a2936bbf1ae76c7fea08702c2e9d03099dfda441
Cihat Uysal <30296073+chtysl@users.noreply.github.com>
2022-11-25 01:31:10 +0300

    Update Navbar.svelte

--- src/lib/Navbar.svelte
+++ src/lib/Navbar.svelte
@@ -6,9 +6,9 @@
 	<div class="flex-1">
 		<a on:click={() => goto('/')} class="btn btn-ghost normal-case text-xl">Cihat Uysal</a>
 	</div>
-	<div class="flex-none">
+	/* <div class="flex-none">
 		<button class="btn btn-square btn-ghost">
 			<a on:click={() => goto('/blog')}>Blog</a>
 		</button>
-	</div>
+	</div> */
 </div>
